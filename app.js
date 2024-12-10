35c84eb341d0a9dcc48f334d7f61a762d52dbac1
souvik <souvikofficial21@gmail.com>
2024-12-10 18:42:16 +0530

    webhook-shopify

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const crypto = require("crypto");
+const fs = require("fs");
+const dotenv = require("dotenv");
+const port = 4000;
+const randomkey = crypto.randomBytes(32).toString("hex");
+const SHOPIFY_API_KEY = process.env.SHOPIFY_SECRET_KEY;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/webhook", (req, res) => {
+  const productData = req.body;
+  console.log("Received webhook:", productData);
+  console.log(productData);
+  fs.writeFileSync(`productData-${randomkey}.json`, JSON.stringify(req.body), {
+    encoding: "utf8",
+    flag: "w",
+  });
+  res.status(200).send("Webhook received");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
